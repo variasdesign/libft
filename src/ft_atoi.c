e8f0832f8265e7d817bbc742aa874d1d5bd9eb78
varias-c <varias-c@student.42malaga.com>
2025-04-18 15:08:50 +0200

    change atois func from static to lib

--- src/ft_atoi.c
+++ src/ft_atoi.c
@@ -6,23 +6,11 @@
 /*   By: varias-c <varias-c@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 17:56:23 by varias-c          #+#    #+#             */
-/*   Updated: 2024/10/23 17:56:50 by varias-c         ###   ########.fr       */
+/*   Updated: 2025/04/18 15:08:28 by varias-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-static int	isdigit(int c)
-{
-	return (!(c < 48 || c > 57));
-}
-
-static int	isspace(int c)
-{
-	if ((c < 9 || c > 13) && c != 32)
-		return (0);
-	return (1);
-}
 
 int	ft_atoi(const char *str)
 {
@@ -33,7 +21,7 @@ int	ft_atoi(const char *str)
 	sign = 1;
 	result = 0;
 	i = 0;
-	while (isspace(str[i]))
+	while (ft_isspace(str[i]))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
 	{
@@ -41,7 +29,7 @@ int	ft_atoi(const char *str)
 			sign = -sign;
 		i++;
 	}
-	while (str[i] && (isdigit(str[i])))
+	while (str[i] && (ft_isdigit(str[i])))
 	{
 		result = result * 10 + (str[i] - '0');
 		i++;

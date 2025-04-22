b852bc28b2d127c5b774b1efcceed7adf944ba61
varias-c <varias-c@student.42malaga.com>
2025-04-22 19:41:08 +0200

    add memset for int arrays

---
+++ src/ft_intset.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: varias-c <varias-c@student.42malaga.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/24 19:30:22 by varias-c          #+#    #+#             */
+/*   Updated: 2024/09/24 19:47:15 by varias-c         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_intset(void *b, int c, size_t len)
+{
+	int	*temp;
+
+	temp = (int *) b;
+	while (len > 0)
+	{
+		*temp = c;
+		temp++;
+		len--;
+	}
+	return (b);
+}
+
+int	main()
+{
+	int *test_memset = malloc(sizeof(int) * 10);
+	int *test_intset = malloc(sizeof(int) * 10);
+	
+	test_memset = ft_memset(test_memset, -1, 10);
+	test_intset = ft_intset(test_intset, -1, 10);
+}

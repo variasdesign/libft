740ce090507b766900240069cab241dea34df5fa
variasdesign <variasdesign@pm.me>
2025-11-21 15:25:17 +0100

    add more robust safety checks for tabcmp

--- src/memory/ft_ptrtab.c
+++ src/memory/ft_ptrtab.c
@@ -6,7 +6,7 @@
 /*   By: varias-c <varias-c@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/06 19:26:23 by varias-c          #+#    #+#             */
-/*   Updated: 2025/10/13 12:33:57 by varias-c         ###   ########.fr       */
+/*   Updated: 2025/11/21 15:16:46 by varias-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,10 +28,21 @@ ssize_t	ft_tabfind(void *ptr, t_ptr_tab tab)
 	return (-1);
 }
 
+// tab1 and tab2 exists and tab1 has pointers
+// but tab2 doesn't have pointers = true
+// tab2 and tab1 exists and tab2 has pointers
+// but tab1 doesn't have pointers = false
+// tab1 exists and has pointers and tab2 exists
+// and has pointers, but t1i is before t2i = true
+// tab1 exists and has pointers and tab2 exists
+// and has pointers, but t2i is before t1i = false
+// tab1 exists and doesn't have pointers
+// and tab2 doesn't have pointers = undefined
 t_bool	ft_tabcmp(t_ptr_tab *tab1, t_ptr_tab *tab2, size_t t1i, size_t t2i)
 {
-	if (!tab2 || (tab1 && tab1->start[t1i]
-		&& (tab1->start[t1i] < tab2->start[t2i] || !tab2->start[t2i])))
+	if ((tab1 && tab2) && (tab1->start) && (tab1->start[t1i])
+		&& (!tab2->start || !tab2->start[t2i]
+			|| tab1->start[t1i] < tab2->start[t2i]))
 		return (t);
 	return (f);
 }

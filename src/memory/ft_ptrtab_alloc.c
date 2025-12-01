f931e5ec73a62e01513b13083795faf803fada31
variasdesign <variasdesign@pm.me>
2025-12-01 19:23:41 +0100

    add error checking to taballoc

--- src/memory/ft_ptrtab_alloc.c
+++ src/memory/ft_ptrtab_alloc.c
@@ -6,7 +6,7 @@
 /*   By: varias-c <varias-c@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/13 12:30:19 by varias-c          #+#    #+#             */
-/*   Updated: 2025/11/28 14:26:02 by varias-c         ###   ########.fr       */
+/*   Updated: 2025/12/01 13:40:52 by varias-c         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,21 +33,25 @@ t_ptr_tab	*ft_tabfree(t_ptr_tab **tab, t_bool free_table)
 
 t_ptr_tab	*ft_taballoc(t_ptr_tab *tab, char *orig, size_t size)
 {
-	tab->size = size;
-	tab->orig = orig;
-	tab->read = orig;
-	tab->start = ft_calloc(tab->count + 1, tab->size);
-	if (!tab->start)
+	if (tab)
 	{
-		free (tab);
-		return (NULL);
+		tab->size = size;
+		tab->orig = orig;
+		tab->read = orig;
+		tab->start = ft_calloc(tab->count + 1, tab->size);
+		if (!tab->start)
+		{
+			free (tab);
+			return (NULL);
+		}
+		tab->end = ft_calloc(tab->count + 1, tab->size);
+		if (!tab->end)
+		{
+			free (tab->start);
+			free (tab);
+			return (NULL);
+		}
+		return (tab);
 	}
-	tab->end = ft_calloc(tab->count + 1, tab->size);
-	if (!tab->end)
-	{
-		free (tab->start);
-		free (tab);
-		return (NULL);
-	}
-	return (tab);
+	return (NULL);
 }
